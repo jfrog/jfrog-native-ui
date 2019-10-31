a11613b840424e3254d9120ed23bd870eb00fe91
Nirmal Guna <nirmal.sheva@gmail.com>
2019-10-31 18:11:35 +0530

    Revert "Xray Graph converted to Vue component"" (#4)

--- src/ui_components/packages_native/packages.native.module.js
+++ src/ui_components/packages_native/packages.native.module.js
@@ -3,12 +3,10 @@ import PackageComponent from "./components/package/package.component";
 import PackagesNativeComponent from "./packages.native.component";
 import VersionComponent from "./components/version/version.component";
 import NativeFiltersDropdown from './components/filters_dropdown/native.filters.dropdown';
-import PackageGraphsComponent from "./components/package-graphs/package-graphs.component";
 
 export default angular.module('packages.native', [])
         .component('packagesNativeComponent', new PackagesNativeComponent())
         .component('packagesComponent', new PackagesComponent())
         .component('packageComponent', new PackageComponent())
         .component('versionComponent', new VersionComponent())
-        .component('nativeFiltersDropdown', new NativeFiltersDropdown())
-        .component('packageGraphsComponent', new PackageGraphsComponent());
+        .component('nativeFiltersDropdown', new NativeFiltersDropdown());
